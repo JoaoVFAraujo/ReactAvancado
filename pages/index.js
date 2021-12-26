6935ad8a9e52f849d2008cb4ff82c57340cbdb78
joaofa <joaofa1244@gmail.com>
2021-12-26 15:09:21 -0300

    fix

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">React Avançado / NextJs</a>
+          Welcome to <a href="https://nextjs.org">React Avançado / NextJS</a>
         </h1>
 
         <p className={styles.description}>
